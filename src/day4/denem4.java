0662ee7211ad0963f77a482f67f4804d4ffb0f4a
asya <asyacool922@gmail.com>
2023-11-03 15:57:19 +0300

    commit-4

--- src/day4/denem4.java
+++ src/day4/denem4.java
@@ -2,6 +2,6 @@ package day4;
 
 public class denem4 {
     public static void main(String[] args) {
-        System.out.println("deneme 4 asya branch");
+        System.out.println("deneme 4 asya branch");//deneme4
     }
 }
