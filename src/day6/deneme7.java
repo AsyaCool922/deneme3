c325cf3348e3c44593b896dfe64ac5e7e74c7bda
asya <asyacool922@gmail.com>
2023-11-03 16:20:48 +0300

    commit-5-6

--- src/day6/deneme7.java
+++ src/day6/deneme7.java
@@ -3,6 +3,6 @@ package day6;
 public class deneme7 {
     public static void main(String[] args) {
         System.out.println("deneme7");
-        System.out.println("deneme 8 from asya");
+        System.out.println("deneme 8 from asya");//branch
     }
 }
